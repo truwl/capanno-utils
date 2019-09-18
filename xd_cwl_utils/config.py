e71a8c810b838b54d70e8b2c6c36ce5be034fb35
Karl Sebby <karl.sebby@xdbio.com>
2019-09-18 10:50:38 -0600

    Fixed tool instance file name name matching for validation. 0.2.1 release

--- xd_cwl_utils/config.py
+++ xd_cwl_utils/config.py
@@ -3,6 +3,7 @@ import tempfile
 from pathlib import Path
 
 test_rel_content_path = Path.cwd() / 'tests' / 'test_files' / 'test_content'
+travis_root = Path('/home/travis/build/xDBio-Inc/cwl-source')
 content_base_path = Path.cwd()
 
 # Default values for using tool in repos.
@@ -20,6 +21,14 @@ _test = {
     'cwl_script_dir': test_rel_content_path / 'cwl-scripts',
     'cwl_workflows_dir': test_rel_content_path / 'cwl-workflows',
     'content_maps_dir': test_rel_content_path / 'content_maps',
+    'temp_dir': tempfile.TemporaryDirectory(prefix='cwlTest_'),
+}
+
+_travis = {
+    'cwl_tool_dir': travis_root / 'cwl-tools',
+    'cwl_script_dir': travis_root / 'cwl-scripts',
+    'cwl_workflows_dir': travis_root / 'cwl-workflows',
+    'content_maps_dir': travis_root / 'content_maps',
     'temp_dir': tempfile.TemporaryDirectory(prefix='cwlTest_'),
 }
 
