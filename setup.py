6a669896ddc61394211cd0df60b24719fded1d6f
Karl Sebby <karl.sebby@xdbio.com>
2019-10-03 09:18:07 -0600

    0.4.0a2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from semantic_version import Version
 with open('README.md', 'r') as fh:
     long_description = fh.read()
 
-package_version = Version(major=0, minor=4, patch=0, prerelease=('alpha', '1'))
+package_version = Version(major=0, minor=4, patch=0, prerelease=('alpha', '2'))
 
 setup(
     name='xd_cwl_utils',
