f9e757cc52343c3096cc951f9804082e08c24a37
Jeremy Leipzig <leipzig@gmail.com>
2020-06-17 14:18:17 -0400

    start the parser

---
+++ xd_cwl_utils/helpers/get_bio_cwl_tools.py
@@ -0,0 +1,5 @@
+""" Get projects from https://github.com/common-workflow-library/bio-cwl-tools
+    make sure this repo is added as a submodule so we can just parse it locally instead of getting bogged down in scraping
+    git submodule add git@github.com:common-workflow-library/bio-cwl-tools.git bio-cwl-tools-submodule
+"""
+
