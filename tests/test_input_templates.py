55a0a4ab7459b02fcd023a0b13d481ff494e1fe2
Karl Sebby <karl.sebby@xdbio.com>
2020-08-24 15:11:36 -0600

    Added function to return workflow maps as a dict. Will need to do the same thing for tools and scripts.

--- tests/test_input_templates.py
+++ tests/test_input_templates.py
@@ -17,7 +17,7 @@ class TestMakeCommandLineToolInputsTemplate(TestBase):
         inputs_schema = InputsSchema(cwl_tool)
         template = inputs_schema.make_template()
         yaml = YAML()
-        with NamedTemporaryFile(delete=True, prefix='template_', suffix='.yml') as tmp:
+        with NamedTemporaryFile(delete=False, prefix='template_', suffix='.yml') as tmp:
             yaml.dump(template, tmp)
             assert True
         return
